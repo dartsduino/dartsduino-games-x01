3303eb08fd38462c052fc6d4ce3716eb34482090
Ikuo Terado <eqobar@gmail.com>
2014-03-01 14:45:01 +0900

    Initial commit

---
+++ lib/dartsduino/games/x01.rb
@@ -0,0 +1,9 @@
+require "dartsduino/games/x01/version"
+
+module Dartsduino
+  module Games
+    module X01
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'dartsduino/games/x01'
